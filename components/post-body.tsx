c720980136fe44e13833d0e6890b362740dc938f
Azerpas <xskylix@hotmail.fr>
2022-05-16 14:19:09 +0000

    Finalized 1st article

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown'
+import CodeBlock from './code-block'
 import markdownStyles from './markdown-styles.module.css'
 
 type Props = {
@@ -8,7 +9,11 @@ type Props = {
 const PostBody = ({ content }: Props) => {
   return (
     <div className="max-w-2xl mx-auto">
-      <ReactMarkdown children={content} className={markdownStyles['markdown']} /> 
+      <ReactMarkdown 
+        children={content} 
+        className={markdownStyles['markdown']}
+        components={CodeBlock}
+      /> 
     </div>
   )
 }
