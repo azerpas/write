bd946df32308894c9a6cc211c8fe3007e359bb4b
azerpas <xskylix@hotmail.fr>
2022-06-29 14:55:53 +0200

    Added GitHub link

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,11 +1,22 @@
 const Intro = () => {
-  return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        writing.
-      </h1>
-    </section>
-  )
+    return (
+        <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+            <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+                writing.
+            </h1>
+            <span className="flex-col">
+                <h5>
+                    <a 
+                        href="https://github.com/azerpas" 
+                        rel="noopener noreferrer" target="_blank"
+                        className="underline decoration-sky-500/30"
+                    >
+                        GitHub
+                    </a>
+                </h5>
+            </span>
+        </section>
+    )
 }
 
 export default Intro
